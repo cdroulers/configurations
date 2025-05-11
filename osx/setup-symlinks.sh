83636a456cde8fcd74350566405467a987001d16
Christian Droulers <droulers.christian@gmail.com>
2025-05-10 21:59:58 -0400

    Add zsh config

--- osx/setup-symlinks.sh
+++ osx/setup-symlinks.sh
@@ -6,3 +6,4 @@ ROOT_DIR=$(realpath $SCRIPT_DIR/..)
 sudo ln -sfn "$ROOT_DIR/nvim" ~/.config/nvim
 sudo ln -sfn "$ROOT_DIR/alacritty" ~/.config/alacritty
 sudo ln -sfn "$ROOT_DIR/tmux/.tmux.conf" ~/.tmux.conf
+sudo ln -sfn "$ROOT_DIR/zsh/.zprofile" ~/.zprofile
