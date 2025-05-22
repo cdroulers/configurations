4235b61b6120b15653358a5a2c7faaa598d04854
Christian Droulers <droulers.christian@gmail.com>
2025-05-22 11:46:06 -0400

    Add vertical guides

--- nvim/init.lua
+++ nvim/init.lua
@@ -103,6 +103,8 @@ vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
+vim.opt.colorcolumn = '80,100,120'
+vim.api.nvim_set_hl(0, 'ColorColumn', { bg = 'Black' })
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
