43ab985a6af6cca132847d7805e558483679d767
Christian Droulers <droulers.christian@gmail.com>
2025-05-23 21:14:40 -0400

    Properly configure alacritty and tmux on linux and osx

--- linux/setup-symlinks.sh
+++ linux/setup-symlinks.sh
@@ -11,3 +11,5 @@ sudo ln -sfn "$ROOT_DIR/zsh/.zshrc" ~/.zshrc
 sudo ln -sfn "$ROOT_DIR/zsh/.p10k.zsh" ~/.p10k.zsh
 sudo ln -sfn "$ROOT_DIR/zsh/oh-my-zsh" ~/.oh-my-zsh
 
+# Link tmux in a safe spot so `alacritty.toml` refers to the same path on all OS
+sudo ln -sf "/usr/bin/tmux" "/usr/local/bin/tmux"
