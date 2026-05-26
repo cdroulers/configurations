86c377ef96ea15e518ec66180f7a9396061dfd6f
Christian Droulers <droulers.christian@gmail.com>
2026-05-26 11:37:20 -0400

    Update for oxfmt and prettier node_modules path

--- nvim/init.lua
+++ nvim/init.lua
@@ -919,49 +919,51 @@ require('lazy').setup({
         desc = '[F]ormat buffer',
       },
     },
-    opts = {
-      notify_on_error = false,
-      formatters = {
-        oxfmt = {
-          command = 'oxfmt',
-          args = { '--stdin-filepath', '$FILENAME' },
-          stdin = true,
-          prefer_local = 'node_modules/.bin',
+    opts = function()
+      local util = require('conform.util')
+
+      return {
+        notify_on_error = false,
+        formatters = {
+          oxfmt = {
+            command = util.from_node_modules('oxfmt'),
+            args = { '--stdin-filepath', '$FILENAME' },
+            stdin = true,
+          },
+          prettier = {
+            command = util.from_node_modules('prettier'),
+          },
         },
-      },
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { 'oxfmt', 'prettier', stop_after_first = true },
-        javascriptreact = { 'oxfmt', 'prettier', stop_after_first = true },
-        typescript = { 'oxfmt', 'prettier', stop_after_first = true },
-        typescriptreact = { 'oxfmt', 'prettier', stop_after_first = true },
-        vue = { 'oxfmt', 'prettier', stop_after_first = true },
-        json = { 'oxfmt', 'prettier', stop_after_first = true },
-        jsonc = { 'oxfmt', 'prettier', stop_after_first = true },
-        yml = { 'oxfmt', 'prettier', stop_after_first = true },
-        yaml = { 'oxfmt', 'prettier', stop_after_first = true },
-        markdown = { 'oxfmt', 'prettier', stop_after_first = true },
-        sql = { 'sqlfluff', stop_after_first = true },
-      },
-    },
+        format_on_save = function(bufnr)
+          -- Disable "format_on_save lsp_fallback" for languages that don't
+          -- have a well standardized coding style. You can add additional
+          -- languages here or re-enable it for the disabled ones.
+          local disable_filetypes = { c = true, cpp = true }
+          if disable_filetypes[vim.bo[bufnr].filetype] then
+            return nil
+          else
+            return {
+              timeout_ms = 500,
+              lsp_format = 'fallback',
+            }
+          end
+        end,
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          javascript = { 'oxfmt', 'prettier', stop_after_first = true },
+          javascriptreact = { 'oxfmt', 'prettier', stop_after_first = true },
+          typescript = { 'oxfmt', 'prettier', stop_after_first = true },
+          typescriptreact = { 'oxfmt', 'prettier', stop_after_first = true },
+          vue = { 'oxfmt', 'prettier', stop_after_first = true },
+          json = { 'oxfmt', 'prettier', stop_after_first = true },
+          jsonc = { 'oxfmt', 'prettier', stop_after_first = true },
+          yml = { 'oxfmt', 'prettier', stop_after_first = true },
+          yaml = { 'oxfmt', 'prettier', stop_after_first = true },
+          markdown = { 'oxfmt', 'prettier', stop_after_first = true },
+          sql = { 'sqlfluff', stop_after_first = true },
+        },
+      }
+    end,
   },
 
   { -- Autocompletion
